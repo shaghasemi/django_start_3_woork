d032a71dbef36237fe1c6e756986eb4685f0a35a
shaghasemi <shaghasemi@gmail.com>
2022-08-13 15:13:32 +0430

    202208131513

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('<int:pk>/', views.product_detail_view)]
+urlpatterns = [
+    path("", views.product_create_view),
+    path("<int:pk>/", views.product_detail_view),
+]
--- backend/products/views.py
+++ backend/products/views.py
@@ -3,6 +3,24 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        # serializer.save()
+        print(serializer.validated_data)
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content")
+        # or None
+        if content is None:
+            content = title
+        serializer.save(content= content)
+
+
+product_create_view = ProductCreateAPIView.as_view()
+
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
 
