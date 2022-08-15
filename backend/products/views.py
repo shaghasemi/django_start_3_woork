8ba0e4abe0e2aacb280c20c4a30256997b2254bf
shaghasemi <shaghasemi@gmail.com>
2022-08-15 11:04:16 +0430

    not found 2

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.decorators import api_view
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
@@ -9,7 +13,7 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         # serializer.save()
-        print(serializer.validated_data)
+        # print(serializer.validated_data)
         title = serializer.validated_data.get("title")
         content = serializer.validated_data.get("content")
         # or None
@@ -23,8 +27,22 @@ product_list_create_view = ProductListCreateAPIView.as_view()
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
-
     serializer_class = ProductSerializer
 
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+
+@api_view(["POST"])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+    
+    if method =="GET":
+        if pk is not None:
+            obj = get_object_or_404
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # instance = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"invalid": "Bad data"})
