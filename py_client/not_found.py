6e65dfe4eee6155464c67fb08186bbfe9b44ba2b
shaghasemi <shaghasemi@gmail.com>
2022-08-15 11:12:28 +0430

    not found 3

--- py_client/not_found.py
+++ py_client/not_found.py
@@ -5,4 +5,4 @@ endpoint = "http://127.0.0.1:8000/api/products/32132/"
 get_response = requests.get(endpoint)
 
 print(get_response.json())
-print(get_response.status_code)
+# print(get_response.status_code)
