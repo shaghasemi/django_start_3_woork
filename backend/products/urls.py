b7be5c3d094cd4a9e8851cbe91c9560f6cc2e183
shaghasemi <shaghasemi@gmail.com>
2022-08-16 11:40:46 +0430

    starting update and destroy

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.product_list_create_view),
-    path("<int:pk>/", views.product_detail_view),
+    path('<int:pk>/', views.product_detail_view),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -33,16 +33,28 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
 product_detail_view = ProductDetailAPIView.as_view()
 
 
-@api_view(["POST"])
+@api_view(['GET', 'POST'])
 def product_alt_view(request, pk=None, *args, **kwargs):
     method = request.method
-    
-    if method =="GET":
+
+    if method == "GET":
         if pk is not None:
-            obj = get_object_or_404
-    serializer = ProductSerializer(data=request.data)
-    if serializer.is_valid(raise_exception=True):
-        # instance = serializer.save()
-        print(serializer.data)
-        return Response(serializer.data)
-    return Response({"invalid": "Bad data"})
+            # Default View
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, data=request.data)
+            return Response(data)
+        # List View
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+    if method == 'POST':
+        # Create an item
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content')
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid": "Not good data"}, status=400)
