cd47bac5cd09fae0b6b197937fde627d90d331b0
shaghasemi <shaghasemi@gmail.com>
2022-08-18 12:54:25 +0430

    custom auth token

--- py_client/create.py
+++ py_client/create.py
@@ -2,12 +2,14 @@ import requests
 
 endpoint = "http://127.0.0.1:8000/api/products/"
 
+headers = {"Authorization": 'Bearer 14980a8ff704fef073b8d2905e5e13046a6d93f1'}
+
 data = {
     "title": "New New",
     "price": 958,
 }
 
-get_response = requests.post(endpoint, json=data)
+get_response = requests.post(endpoint, json=data, headers=headers)
 
 print(get_response.json())
 print(get_response.status_code)
